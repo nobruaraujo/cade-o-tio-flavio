480256285c2f7186026a52ac49201b0528e5ef36
Bruno Araujo <brunosousa357@gmail.com>
2025-03-22 15:50:39 -0300

    First commit

---
+++ src/main/java/br/com/nobru/cade_o_tio_flavio/CadeOTioFlavioApplication.java
@@ -0,0 +1,13 @@
+package br.com.nobru.cade_o_tio_flavio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadeOTioFlavioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadeOTioFlavioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nobru/cade_o_tio_flavio/CadeOTioFlavioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nobru.cade_o_tio_flavio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadeOTioFlavioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
